87e1730855e1d76c93a5ca82b244067b7a77b288
kazuki-cpu <84381474+kazuki-cpu@users.noreply.github.com>
2024-07-08 18:03:36 +0900

    Update and rename Tracer.hpp.txt to Tracer.hpp

--- Tracer/Tracer.hpp
+++ Tracer/Tracer.hpp
@@ -1,17 +1,22 @@
-#include "Color.hpp" 
-#include "util.h"        
+#include "Motor.hpp"
+#include "Color.hpp"
+#include "util.h"
 
 /* ライントレースエッジ切り替え */
 #define LEFT_EDGE         (-1)
 #define RIGHT_EDGE        (1)
 
 /* 走行基準スピード */
-#define BASE_SPEED        (40)//←これ変数にした方がいい？
 
 /* ステアリング操舵量の係数 */
 #define KP         (0.83)//←これ変数にした方がいい？
 
 using namespace ev3api;
+
+enum Mode {
+  Straight,
+  Curve
+};
 
 class Tracer {
 public:
@@ -20,11 +25,15 @@ public:
   extern void tracer_task(intptr_t exinf);
   void init();
   void terminate();
-  //set_parameters(,,);mode, base_speed, Kp, biasを変更する関数(未実装)
+  set_parameters(int base_speed, double Kp, int bias);, base_speed, Kp, biasを変更する関数(未実装)
   motor_control();
 
 private:
+  int mode;
+  int base_speed = 40;/* 走行基準スピード */
+  double Kp = 0.83;
   int bias = 0;
+  Motor motor;
   Color color;
   const int8_t mThreshold = 20;
 #ifndef MAKE_RASPIKE
