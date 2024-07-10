a2d0d62324335e34b6f692f3ef3e6e71a6031dfa
kazuki-cpu <84381474+kazuki-cpu@users.noreply.github.com>
2024-07-10 17:49:41 +0900

    Update and rename app.hpp to app.h

--- app.h
+++ app.h
@@ -1,9 +1,7 @@
-
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#include "Motor.hpp"
 #include "ev3api.h"
 
 #define MAIN_PRIORITY    (TMIN_APP_TPRI + 1)
@@ -11,7 +9,7 @@ extern "C" {
 #define CALIBRATION_PRIORITY  (TMIN_APP_TPRI + 2) /* キャリブレーションタスク */
 
 /* タスク周期の定義 */
-#define LINE_TRACER_PERIOD  (100 * 1000 / 0.6 ) /* ライントレースタスク:100msec周期 */
+#define TRACER_PERIOD  (100 * 1000 / 0.6 ) /* ライントレースタスク:100msec周期 */
 #define CALIBRATION_PERIOD  (100 * 1000 / 0.6) /* キャリブレーションタスク:100msec周期 */
 
 /* センサーポートの定義 */
@@ -27,17 +25,18 @@ static const sensor_port_t
 
 #ifndef TOPPERS_MACRO_ONLY
 
-Motor motor;
+extern void main_task(intptr_t exinf); /*メインタスク*/
+extern void tracer_task(intptr_t exinf);/* トレーサータスク*/
+extern void calibration_task(intptr_t unused);/* キャリブレーション */
 
-extern void main_task(intptr_t exinf);
-
-static void main_init();     /*初期設定*/
-static void wait_start();/* 動作開始待ち */
-static void calibration();/* キャリブレーション */
-static void p_tracer();/*P制御ライントレース*/
-void wait_msec(int32_t msec);/* タスク停止関数(ミリ秒) */
 
 #endif /* TOPPERS_MACRO_ONLY */
+
+void main_init();     /*初期設定*/
+void wait_start();/* 動作開始待ち */
+void calibration();/* キャリブレーション */
+void p_tracer();/*P制御ライントレース*/
+void wait_msec(int32_t msec);/* タスク停止関数(ミリ秒) */
 
 #ifdef __cplusplus
 }
