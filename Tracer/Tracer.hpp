1e71d25f2381160fd8d6e0e10341749dff879df0
kazuki-cpu <84381474+kazuki-cpu@users.noreply.github.com>
2024-07-21 17:20:41 +0900

    Update Tracer.hpp

--- Tracer/Tracer.hpp
+++ Tracer/Tracer.hpp
@@ -1,6 +1,6 @@
 #include "Motor.hpp"
-#include "Color.hpp"
-#include "util.h"
+#include "Color/Color.hpp"
+#include "ev3.h"
 
 /* ライントレースエッジ切り替え */
 #define LEFT_EDGE         (-1)
@@ -9,38 +9,41 @@
 /* 走行基準スピード */
 
 /* ステアリング操舵量の係数 */
-#define KP         (0.83)//←これ変数にした方がいい？
-
-using namespace ev3api;
+#define KP   　(0.83)
+#define KI         (0.83)
+#define KD         (0.83)
 
 enum Mode {
   Straight,
   Curve
+  End
 };
+enum Mode mode;
 
 class Tracer {
 public:
   Tracer(); //コンストラクタ
 
-  void init();
   void terminate();
-  set_parameters(int base_speed, double Kp, int bias);, base_speed, Kp, biasを変更する関数(未実装)
-  motor_control();
+  //void set_parameters(int base_speed, double Kp, int bias);base_speed, Kp, biasを変更する関数(未実装)
+ float P_steering_amount_calc();
+  float PID_steering_amount_calc();
+  void trace_motor_control(int16_t rotation);
 
 private:
-  int mode;
+ float cur_diff;
+  float pre_diff;
   int base_speed = 40;/* 走行基準スピード */
   double Kp = 0.83;
   int bias = 0;
   Motor motor;
   Color color;
   const int8_t mThreshold = 20;
+/*
 #ifndef MAKE_RASPIKE
   const int8_t pwm = (Motor::PWM_MAX) / 6;
 #else
   const int8_t pwm = 60;
 #endif
-
- int16_t P_steering_amount_calc();
-
+*/
 };
