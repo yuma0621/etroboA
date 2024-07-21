1ebb2d86972f579fa3ee14591e8a219c56178384
kazuki-cpu <84381474+kazuki-cpu@users.noreply.github.com>
2024-07-21 17:51:27 +0900

    Update Motor.hpp

--- Motor/Motor.hpp
+++ Motor/Motor.hpp
@@ -1,17 +1,42 @@
 using namespace ev3api;
+/* 円周率 */
+#define PI 3.14159265358
 
 class Motor{
 public:
   Motor(); //コンストラクタ
 
   void terminate();
-  void motor_control(int left_motor_power, int right_motor_power);
+  void motor_control();
+  void Distance_reset();
+  void Distance_update();
+  void Distance_getDistance();
+  void Direction_reset();
+  float Direction_getDirection();
+  void Direction_update()
+
+ // 方位を更新
+void Direction_update();
+
+/* 方位を取得(右旋回が正転) */
+float Direction_getDirection();
+
+ // 方位を更新
+void Direction_update();
 
  /* モーターポートの定義 */
 static const motor_port_t
     arm_motor       = EV3_PORT_A,
     left_motor      = EV3_PORT_C,
     right_motor     = EV3_PORT_B;
+
+   static floatdistance;
+   static float distance4msR;
+   static float distance4msL;
+   static float pre_angleL;
+   static float pre_angleR;
+   static float distance4ms;
+   static float direction; //現在の方位
 
 private:
   const int8_t mThreshold = 20;
@@ -20,5 +45,4 @@ private:
 #else
   const int8_t pwm = 60;
 #endif
-
 };
