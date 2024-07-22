42f6edc3875740c14724c1b4991346ac6bc0f33e
kazuki-cpu <84381474+kazuki-cpu@users.noreply.github.com>
2024-07-22 21:39:30 +0900

    Update Color.hpp

--- Color/Color.hpp
+++ Color/Color.hpp
@@ -1,11 +1,11 @@
-#include "ev3.h"
+#include "ev3api.h"
 
-//using namespace ev3api;
+using namespace ev3api;
 
 class Color {
 public:
   Color(); //コンストラクタ
-  static int16_t get_rgb_diff();
+  int16_t get_rgb_diff();
   //void init();←コンストラクタで十分か?
   //void terminate();
 
