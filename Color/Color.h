39d6972dae2533a5a353d0e117a3a54a1f379f1d
kazuki-cpu <84381474+kazuki-cpu@users.noreply.github.com>
2024-07-23 17:47:35 +0900

    Update Color.h

--- Color/Color.h
+++ Color/Color.h
@@ -1,15 +1,19 @@
+#pragma once
+
 #include "ev3api.h"
 
-using namespace ev3api;
+//using namespace ev3api;
 
 //public:
-  color_init(); //コンストラクタ
-  int16_t get_rgb_diff();
+  void color_init(); //コンストラクタ
+  int16_t color_get_rgb_diff();
   //void init();←コンストラクタで十分か?
   //void terminate();
 
 /* RGB値 */
+/* センサーポートの定義 */
+sensor_port_t color_sensor;
+
 rgb_raw_t rgb_max;
 rgb_raw_t rgb_min;
 rgb_raw_t rgb_ave;
-};
