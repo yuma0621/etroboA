8d121dcf850f4980592262f8bbd78bef57e419fe
kazuki-cpu <84381474+kazuki-cpu@users.noreply.github.com>
2024-08-04 14:39:30 +0900

    Update Color.h

--- Color/Color.h
+++ Color/Color.h
@@ -1,19 +1,23 @@
-#pragma once
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-#include "ev3api.h"
+/* シミュレータ */
+#ifdef ETROBOC_SIM
+    #define RGB_GAIN_RED        1.0     /* RGBセンサ補正値 */
+    #define RGB_GAIN_GREEN      1.0     /* RGBセンサ補正値 */
+    #define RGB_GAIN_BLUE       0.7     /* RGBセンサ補正値 */
+/* RasPike実機 */
+#else
+    #define RGB_GAIN_RED        1.0     /* RGBセンサ補正値 */
+    #define RGB_GAIN_GREEN      1.0     /* RGBセンサ補正値 */
+    #define RGB_GAIN_BLUE       1.0     /* RGBセンサ補正値 */
+#endif
 
-//using namespace ev3api;
+/* 関数プロトタイプ宣言 */
+void adjust_rgb(rgb_raw_t *rgb_raw);
+void rgb2hsv(rgb_raw_t rgb_raw, int16_t *h, int16_t *s, int16_t *v);
 
-//public:
-  void color_init(); //コンストラクタ
-  int16_t color_get_rgb_diff();
-  //void init();←コンストラクタで十分か?
-  //void terminate();
-
-/* RGB値 */
-/* センサーポートの定義 */
-sensor_port_t color_sensor;
-
-rgb_raw_t rgb_max;
-rgb_raw_t rgb_min;
-rgb_raw_t rgb_ave;
+#ifdef __cplusplus
+}
+#endif
