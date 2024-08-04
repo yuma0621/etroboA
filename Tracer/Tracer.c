a95ffb352ec8858c5f338712a140562ba4e8663d
kazuki-cpu <84381474+kazuki-cpu@users.noreply.github.com>
2024-08-04 22:57:17 +0900

    Update Tracer.c

--- Tracer/Tracer.c
+++ Tracer/Tracer.c
@@ -11,7 +11,7 @@ typedef enum {
 
 void tracer_task(intptr_t unused) {
     static RUN_STATE state = AHEAD;
-    static RUN_STATE state = TURN;
+    //static RUN_STATE state = TURN;
 
     /* 計測器初期化 */
     //Distance_reset();
