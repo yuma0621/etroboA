d262f12cc4c7588f2c5f7a786032e7a006c390b6
kazuki-cpu <84381474+kazuki-cpu@users.noreply.github.com>
2024-08-04 14:38:07 +0900

    Update Color.c

--- Color/Color.c
+++ Color/Color.c
@@ -1,22 +1,35 @@
+#include <stdio.h>
+#include "app.h"
 #include "Color.h"
-//#include <stdio.h>
 
-void color_init(){
-    color_sensor = EV3_PORT_2;
-    rgb_max.r = 0;
-    rgb_min.r = 200;
-    rgb_max.g = 0;
-    rgb_min.g = 200;
-    rgb_max.b = 0;
-    rgb_min.b = 200;
+/* RGB値補正関数 */
+ void adjust_rgb(rgb_raw_t *rgb_raw){
+    /* RGB値補正（シミュレーション向けの補正値） */
+    (*rgb_raw).r = (uint16_t)((*rgb_raw).r * RGB_GAIN_RED);
+    (*rgb_raw).g = (uint16_t)((*rgb_raw).g * RGB_GAIN_GREEN);
+    (*rgb_raw).b = (uint16_t)((*rgb_raw).b * RGB_GAIN_BLUE);
 }
 
-/* 反射光とキャリブレーション値との差を取得*/
-int16_t color_get_rgb_diff(){
-    rgb_raw_t rgb_raw;
-    int16_t green_diff;
-    ev3_color_sensor_get_rgb_raw(color_sensor, &rgb_raw);       /* カラーセンサ値の取得 */
-    green_diff = rgb_ave.g - rgb_raw.g;                     /* キャリブレーション値との差分を計算(green) */
-    return green_diff;
+/* RGB値からHSV値に変換する関数 */
+void rgb2hsv(rgb_raw_t rgb_raw, int16_t *h, int16_t *s, int16_t *v){
+    int16_t max,min;
+    /* 最大値取得 */
+    max = rgb_raw.r;                
+    if (rgb_raw.g > max) max = rgb_raw.g;
+    if (rgb_raw.b > max) max = rgb_raw.b;
+    /* 最小値取得 */
+    min = rgb_raw.r;
+    if (rgb_raw.g < min) min = rgb_raw.g;
+    if (rgb_raw.b < min) min = rgb_raw.b;
+    /* 色相H計算 */
+    if(rgb_raw.r == max) *h = 60 * (rgb_raw.g - rgb_raw.b) / (max - min);
+    if(rgb_raw.g == max) *h = 60 * (rgb_raw.b - rgb_raw.r) / (max - min) + 120;
+    if(rgb_raw.b == max) *h = 60 * (rgb_raw.r - rgb_raw.g) / (max - min) + 240;
+    /* 彩度S計算 */
+    *s = 255 * (max - min) / max;
+    /* 明度V計算 */
+    *v = max;
+    /* 結果表示 */
+    printf("rgbhsv=, %3d, %3d, %3d, %3d, %3d, %3d, ",rgb_raw.r, rgb_raw.g, rgb_raw.b,*h,*s,*v);
 }
 
