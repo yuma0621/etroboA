12a4562db003bd22c3919942c0d89da9f86c18d2
kazuki-cpu <84381474+kazuki-cpu@users.noreply.github.com>
2024-08-04 22:34:35 +0900

    Update Tracer.c

--- Tracer/Tracer.c
+++ Tracer/Tracer.c
@@ -5,22 +5,27 @@ typedef enum {
 } RUN_STATE;
 
 void tracer_task(intptr_t unused) {
+    static RUN_STATE state = AHEAD;
     static RUN_STATE state = TURN;
 
     /* 計測器初期化 */
-    Distance_reset();
-    Direction_reset();
+    //Distance_reset();
+    //Direction_reset();
 
-    while(1) {
-        /* 計測器更新 */
-        Distance_update();
-        Direction_update();
-
-        switch(state) {
+    switch(state) {
+        case AHEAD:
+            //左右車輪駆動
+            ev3_motor_set_power(left_motor, 30);
+            ev3_motor_set_power(right_motor, 30);
+            //400mm以上前進したら，次状態遷移
+            if(Distance_getDistance() > 400.0) {
+                state = END;
+            }
+            break;
         case TURN:
             ev3_motor_set_power(left_motor, 30);
             ev3_motor_set_power(right_motor, -30);
-            if( Direction_getDirection() > 180.0) {;
+            if( Direction_getDirection() > 90.0) {;
                 state = END;
             }
             break;
@@ -30,6 +35,6 @@ void tracer_task(intptr_t unused) {
             break;
         default:
             break;
-        }
     }
 }
+
