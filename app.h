44e08ef9fde051f451927218c0f2fc9eab13ee74
kazuki-cpu <84381474+kazuki-cpu@users.noreply.github.com>
2024-08-04 21:36:00 +0900

    Update app.h

--- app.h
+++ app.h
@@ -10,12 +10,6 @@ extern "C" {
 #endif
 
 /* グローバル変数 */
-extern int16_t s_min;          /* 彩度Sの平均値（目標値） */
-extern int16_t s_max;          /* 彩度Sの平均値（目標値） */
-extern int16_t s_ave;          /* 彩度Sの平均値（目標値） */
-extern int16_t v_min;          /* 明度Vの平均値（目標値） */
-extern int16_t v_max;          /* 明度Vの平均値（目標値） */
-extern int16_t v_ave;          /* 明度Vの平均値（目標値） */
 
 /* センサーポートの定義 */
 static const sensor_port_t
@@ -38,30 +32,22 @@ static const motor_port_t
 /* タスクのプロトタイプ宣言 */
 #ifndef TOPPERS_MACRO_ONLY
     extern void main_task(intptr_t exinf);              /* メインタスク(起動時にのみ関数コールされる) */
-    extern void v_sensor_calib_task(intptr_t unused);   /* 光センサV値キャリブレーションタスク）(10msec周期) */
-    extern void s_sensor_calib_task(intptr_t unused);   /* 光センサS値キャリブレーションタスク）(10msec周期) */
-    extern void tracer_task(intptr_t exinf);            /* ライントレースタスク(10msec周期） */
     extern void odometry_task(intptr_t exinf); 
 #endif
 
 /* タスク優先度 */
 #define MAIN_PRIORITY    (TMIN_APP_TPRI + 1)
-#define V_CALIB_PRIORITY  (TMIN_APP_TPRI + 2)
-#define S_CALIB_PRIORITY  (TMIN_APP_TPRI + 2)
-#define TRACER_PRIORITY  (TMIN_APP_TPRI + 2)
 #define ODOMETRY_PRIORITY  (TMIN_APP_TPRI + 2)
 
 /* タスク周期 */
 #ifdef ETROBOC_SIM  /* シミュレータ */
     #define LINE_TRACER_PERIOD  (10 * 1000 / 0.6 )
-    #define V_CALIB_PERIOD  (10 * 1000 / 0.6 )
-    #define S_CALIB_PERIOD  (10 * 1000 / 0.6 )
     #define ODOMETRY_PERIOD  (10 * 1000 / 0.6 )
 #else   /* RasPike実機 */
     #define LINE_TRACER_PERIOD  (10 * 1000 )
     #define V_CALIB_PERIOD  (10 * 1000 )
     #define S_CALIB_PERIOD  (10 * 1000 )
-    #define ODOMETRY_PERIOD  (10 * 1000 / 0.6 )
+    #define ODOMETRY_PERIOD  (10 * 1000 )
 #endif
 
 /* 関数のプロトタイプ宣言 */
