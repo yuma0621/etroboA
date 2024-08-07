f631c1b88b9bb22ffcbeced07708217c71619700
kazuki-cpu <84381474+kazuki-cpu@users.noreply.github.com>
2024-08-07 21:04:15 +0900

    Update Tracer.c

--- Tracer/Tracer.c
+++ Tracer/Tracer.c
@@ -21,7 +21,7 @@ void tracer_task(intptr_t unused) {
     switch(state) {
         case AHEAD:
             //左右車輪駆動
-            ev3_motor_set_power(left_motor, 49);
+            ev3_motor_set_power(left_motor, 50);
             ev3_motor_set_power(right_motor, 45);
             //1000mm以上前進したら，次状態遷移
             if(odom_Distance_getDistance() > 1000.0) {
