b70bcb2d183fbd66f242b41a8723a71fc82426fb
kazuki-cpu <84381474+kazuki-cpu@users.noreply.github.com>
2024-08-14 23:06:41 +0900

    Update Tracer.c

--- Tracer/Tracer.c
+++ Tracer/Tracer.c
@@ -29,8 +29,8 @@ void tracer_task(intptr_t unused) {
             }
             break;
         case TURN:
-            ev3_motor_set_power(left_motor, 64);
-            ev3_motor_set_power(right_motor, -40);
+            ev3_motor_set_power(left_motor, 60);//64
+            ev3_motor_set_power(right_motor, -37);//40
             if(odom_Direction_getDirection() > 90.0) {;
                 state = END;
             }
